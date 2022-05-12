23d4c856819dcf535d6495d07f4909a2f6cacea5
von <xanskrit@gmail.com>
2022-05-13 07:58:59 +0800

    committed on 130522.0759

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function openLogin() {
-    document.getElementById("login").style.display = "block";
+function openContainer() {
+    const loginContainer = document.getElementById("login-container");
+    loginContainer.style.display = "flex";
+    loginContainer.style.pointerEvents = "auto";
+    document.getElementById("body").style.overflow = "hidden";
+
+}
+
+function closeContainer() {
+    const loginContainer = document.getElementById("login-container");
+    loginContainer.style.display = "none";
+    loginContainer.style.pointerEvents = "none";
+    document.getElementById("body").style.overflow = "auto";
 }
