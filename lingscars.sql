6b1b342b1295adda9f2ff66d0449ddddffd323c9
Von <66372745+Von0816@users.noreply.github.com>
2022-06-16 11:59:52 +0800

    Add files via upload

--- lingscars.sql
+++ lingscars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2022 at 05:53 AM
+-- Generation Time: Jun 16, 2022 at 05:59 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -32,69 +32,8 @@ CREATE TABLE `booking` (
   `custID` int(11) NOT NULL,
   `carID` int(11) NOT NULL,
   `bookingDate` int(11) NOT NULL,
-  `bookingTime` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `car`
---
-
-CREATE TABLE `car` (
-  `carID` varchar(16) NOT NULL,
-  `carName` varchar(126) NOT NULL,
-  `carPrice` int(255) NOT NULL,
-  `carBody` varchar(126) NOT NULL,
-  `carTrim` varchar(126) NOT NULL,
-  `carFuel` varchar(64) NOT NULL,
-  `carBHP` int(255) NOT NULL,
-  `carGearBox` varchar(64) NOT NULL,
-  `carPaint` varchar(64) NOT NULL,
-  `carTerm` varchar(10) NOT NULL,
-  `carMonthlyRate` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `customerID` varchar(64) NOT NULL,
-  `customerName` varchar(64) NOT NULL,
-  `customerGender` varchar(10) NOT NULL,
-  `customerAddress` varchar(126) NOT NULL,
-  `customerEmail` varchar(64) NOT NULL,
-  `customerHP` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payment`
---
-
-CREATE TABLE `payment` (
-  `paymentID` varchar(16) NOT NULL,
-  `bookingID` varchar(16) NOT NULL,
-  `paymentDate` date NOT NULL,
-  `paymentTime` time(6) NOT NULL,
-  `paymentMethod` varchar(126) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userID` varchar(64) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `accType` varchar(5) NOT NULL
+  `bookingTime` int(11) NOT NULL,
+  `bookingStatus` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -106,30 +45,6 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`bookingID`);
-
---
--- Indexes for table `car`
---
-ALTER TABLE `car`
-  ADD PRIMARY KEY (`carID`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customerID`);
-
---
--- Indexes for table `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`paymentID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
