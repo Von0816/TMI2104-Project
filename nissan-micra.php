f08080a578c851090cac143a7bf613651e2018cb
Wienns <104183557+Wienns@users.noreply.github.com>
2022-06-16 23:04:26 +0800

    Update and rename nissan-micra.html to nissan-micra.php

---
+++ nissan-micra.php
@@ -0,0 +1,48 @@
+<?php include("connection.php"); ?>
+
+<html>
+    <head>
+        <title>LINGsCARS</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="carInfo.css">
+        <link rel="icon" type="image/x-icon" href="img/logo2.png">
+        <script src="main.js"></script>
+        
+
+    </head>
+    <body id="body">
+        <!--nav bar-->
+        <?php include("navBarIndex.php"); ?>
+ 
+        
+        <section class="CarInfo">
+            <div class="row">
+                <div class="col-1">
+                    <h2>Nissan Micra</h2>
+                    <h3>1.0 IG-T Visia (92bhp) Hatchback 5dr Petrol Manual (92bhp)</h3>
+                    <h3>(Brand New)</h3>
+                    <p>Fuel : Petrol </p>
+                    <p>Paint : Non Metalic</p>
+                    <p>Gearbox : Manual</p>
+                    <h4>£183.77/mth including VAT</h4>
+                    <a href="Checkout.html"><button type="button">Book Now </button>
+                </div>
+                <div class="col-2">
+                    <img src="img\Nissan Micra.jpg" class ="car">
+                    <div class="color-box"></div>
+
+                </div>
+            </div>
+                
+        <br><br><br><br>
+                
+        <!--FOOTER-->
+        <section class="footer">
+            <footer class="f">
+                <a href="carInfo.css"><img src="img/lingscars-logo.png"></a>           
+                <p> Company Reg No: 6178634 || VAT No: 866 0241 30 <br>
+                    © Copyright 2004 - 2022 LINGsCARS.com. All rights reserved.</p>
+            </footer>
+        </section>
+    </body>
+</html>
