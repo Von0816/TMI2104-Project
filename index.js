b9b4b6f4d04c343d2d477b52f77b5b768d8e2130
Wienns <104183557+Wienns@users.noreply.github.com>
2022-06-17 19:35:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,26 @@ function showSlides(n) {
   setTimeout(showSlides, 5000); // Change image every 5 seconds
   
 }
+
+
+
+// Get the login modal
+var modal = document.getElementById('loginID');
+
+// When the user clicks anywhere outside of the login modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+          modal.style.display = "none";
+    }
+}
+
+
+function myLogOutFunction() {
+  if (confirm("Are you sure to log out?")) {
+    window.location='logout.php';
+  } 
+  else{
+    window.history.back();
+  }
+  document.getElementById("logoutID").innerHTML = window.location;
+}
