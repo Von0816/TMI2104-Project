b39c45e233e0c425238a30dddbbf54eaa6af41cd
von <xanskrit@gmail.com>
2022-06-23 01:24:59 +0800

    fix signup

--- signup/addMember.php
+++ signup/addMember.php
@@ -1,6 +1,6 @@
 <?php
 
-  include "connection.php";
+  include "../connection.php";
 
   $memberUsername=$memberName=$memberGender=$memberAddress=$memberEmail=$memberHP=$memberPassword=$memberPassword="";;
  
